0906f691ee1d26b200255b8e78b8091574c78e3f
RAY LEE <kavo1720@gmail.com>
2021-03-23 00:08:14 +0800

    update

--- App.tsx
+++ App.tsx
@@ -1,69 +1,41 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-} from 'react-native';
+import React, { Component } from 'react';
+import './App.css';
 
-const App = () => {
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <TouchableOpacity
-          style={styles.buttonClickStyle}
-          activeOpacity={0.5}>
-          <Image
-            source={{
-              uri:
-                'https://reactjs.org/logo-og.png',
-            }}
-            style={styles.buttonImageIconStyle}
-          />
-          <View style={styles.buttonIconSeparatorStyle} />
-          <Text style={styles.buttonTextStyle}>click me</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
-};
+class App extends Component {
+    constructor(props){
+      super(props)
+      this.state={
+          display:'block',
+          isToggleOn:true
+      };
+      this.bindClick=this.handleClick.bind(this);
+    }
+  handleClick=()=>{
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    margin: 10,
-    marginTop: 30,
-    padding: 30,
-  },
-  buttonClickStyle: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#485a96',
-    borderWidth: 0.5,
-    borderColor: '#fff',
-    height: 40,
-    borderRadius: 5,
-    margin: 5,
-  },
-  buttonImageIconStyle: {
-    padding: 10,
-    margin: 5,
-    height: 25,
-    width: 25,
-    resizeMode: 'stretch',
-  },
-  buttonTextStyle: {
-    color: '#fff',
-    marginBottom: 4,
-    marginLeft: 10,
-  },
-  buttonIconSeparatorStyle: {
-    backgroundColor: '#fff',
-    width: 1,
-    height: 40,
-  },
-});
+        this.setState(
+          prevState =>{
+            return {
+              display: prevState.isToggleOn ? 'none': 'block',
+              isToggleOn : !prevState.isToggleOn
+            }
+          }
+        )
+            
+  }
+  render() {
+    return (
+      <div className="App">
+         <button onClick={this.handleClick}>
+                 {this.state.isToggleOn ? "關閉圖片" : "打開圖片"}
+         </button>
+         <div className="i-test"  
+              style={{display: this.state.display}} 
+         >         
+         <img src={"https://reactjs.org/logo-og.png"}/>
+         </div>               
+      </div>
+    );
+  }
+}
 
 export default App;
